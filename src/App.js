d05e81307ec6e7d08f59ff715653b60010a76d1c
Yehia-Gamal <yahia.elspaa@gmail.com>
2022-11-16 17:00:19 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <section className="App-parent">
+        <h1>Hello React JS</h1>
+      </section>
+    </div>
+  );
+}
+export default App;
