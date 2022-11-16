3574e5d2829b5e062c0abfc686fcc08317000874
Yehia-Gamal <yahia.elspaa@gmail.com>
2022-11-16 17:37:04 +0200

    edit app.js file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
   return (
     <div className="App">
       <section className="App-parent">
+        <h3>version 2</h3>
         <h1>Hello React JS</h1>
       </section>
     </div>
